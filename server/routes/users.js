943c43bc97267a499384ec65828cb14010f9f159
forest-shadow <mamasay@yandex.ru>
2018-10-03 13:01:34 +0300

    Sign Up: Save user in db

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,8 @@
 import express from 'express'
+import bcrypt from 'bcryptjs'
 
 import validateInput from '../shared/validations/signup'
+import User from '../models/user'
 
 let router = express.Router()
 
@@ -8,7 +10,15 @@ router.post('/', (req, res) => {
   const { errors, isValid } = validateInput(req.body)
 
   if (isValid) {
-    res.json({ success: true })
+    const { username, password, timezone, email } = req.body
+    const salt = bcrypt.genSaltSync(10)
+    const password_digest = bcrypt.hashSync(password, salt)
+
+    User.forge({
+      username, timezone, email, password_digest
+    }, { hasTimeStamps: true } ).save()
+      .then(user => res.json({ success: true }))
+      .catch(err => res.status(500).json({ error: err }))
   } else {
     res.status(400).json(errors)
   }
