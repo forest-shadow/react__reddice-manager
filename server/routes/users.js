36776b0164be665648938a8c4f38332615c2e5c8
forest-shadow <mamasay@yandex.ru>
2018-10-04 09:38:01 +0300

    Sign Up: Validate username and email uniqueness on server

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,27 +1,52 @@
 import express from 'express'
 import bcrypt from 'bcryptjs'
+import isEmpty from 'lodash/isEmpty'
 
-import validateInput from '../shared/validations/signup'
+import commonValidations from '../shared/validations/signup'
 import User from '../models/user'
 
 let router = express.Router()
 
+function validateInput(data, otherValidations) {
+  let { errors } = otherValidations(data)
+
+  return User.query({
+    where: { email: data.email },
+    orWhere: { username: data.username }
+  }).fetch().then(user => {
+    if (user) {
+      if(user.get('username') === data.username) {
+        errors.username = 'There is user with such username'
+      }
+
+      if(user.get('email') === data.email) {
+        errors.email = 'There is user with such email'
+      }
+    }
+
+    return {
+      errors,
+      isValid: isEmpty(errors)
+    }
+  })
+}
+
 router.post('/', (req, res) => {
-  const { errors, isValid } = validateInput(req.body)
+  validateInput(req.body, commonValidations).then(({ errors, isValid}) => {
+    if (isValid) {
+      const { username, password, timezone, email } = req.body
+      const salt = bcrypt.genSaltSync(10)
+      const password_digest = bcrypt.hashSync(password, salt)
 
-  if (isValid) {
-    const { username, password, timezone, email } = req.body
-    const salt = bcrypt.genSaltSync(10)
-    const password_digest = bcrypt.hashSync(password, salt)
-
-    User.forge({
-      username, timezone, email, password_digest
-    }, { hasTimeStamps: true } ).save()
-      .then(user => res.json({ success: true }))
-      .catch(err => res.status(500).json({ error: err }))
-  } else {
-    res.status(400).json(errors)
-  }
+      User.forge({
+        username, timezone, email, password_digest
+      }, { hasTimeStamps: true } ).save()
+        .then(user => res.json({ success: true }))
+        .catch(err => res.status(500).json({ error: err }))
+    } else {
+      res.status(400).json(errors)
+    }
+  })
 })
 
 export default router
